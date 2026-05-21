d1594726881323d68a9cb757a5c2e37e27360431
HONGSEOKJU <sj.hong@wjthinkbig.com>
2026-05-21 15:45:35 +0900

    member service.yml 추가

--- member-service/src/main/kotlin/com/ecommerce/member/controller/MemberController.kt
+++ member-service/src/main/kotlin/com/ecommerce/member/controller/MemberController.kt
@@ -1,5 +1,6 @@
 package com.ecommerce.member.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController
 @RefreshScope
 class MemberController() {
 
+    // 3. config-repo에 있는 값을 주입받음
+    @Value("\${member.greeting}")
+    private lateinit var greeting: String
+
     @GetMapping("/greeting")
     fun getGreeting(): String {
-        return "greeting"
+        return greeting
     }
 }
